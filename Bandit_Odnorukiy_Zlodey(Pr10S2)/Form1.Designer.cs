f266c18b3a5831518a8f7179a3f6ca327feacf8a
student <student@C429U40>
2020-11-03 15:36:10 +0300

    Добавьте файлы проекта.

---
+++ Bandit_Odnorukiy_Zlodey(Pr10S2)/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Bandit_Odnorukiy_Zlodey_Pr10S2_
+{
+  partial class Казино
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(119, 112);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(96, 96);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "label1";
+      // 
+      // label2
+      // 
+      this.label2.Location = new System.Drawing.Point(221, 112);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(96, 96);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "label2";
+      // 
+      // label3
+      // 
+      this.label3.Location = new System.Drawing.Point(323, 112);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(96, 96);
+      this.label3.TabIndex = 2;
+      this.label3.Text = "label3";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(72, 256);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 3;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(72, 326);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(121, 23);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // Казино
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(604, 461);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Казино";
+      this.Text = "Азино Три Топора";
+      this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Bandit_Odnorukiy_Zlodey(Pr10S2)/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bandit_Odnorukiy_Zlodey_Pr10S2_
+{
+  public partial class Казино : Form
+  {
+    public Казино()
+    {
+      InitializeComponent();
+    }
+  }
+}
